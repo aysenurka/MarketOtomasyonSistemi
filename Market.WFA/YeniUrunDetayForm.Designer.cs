8060c4495ba776353ca5c4768ccd33ff7f6120af
icadir <ismailcadir@gmail.com>
2019-01-10 09:30:12 +0300

    sss

--- Market.WFA/YeniUrunDetayForm.Designer.cs
+++ Market.WFA/YeniUrunDetayForm.Designer.cs
@@ -61,6 +61,7 @@
             this.label15 = new System.Windows.Forms.Label();
             this.nuAdet = new System.Windows.Forms.NumericUpDown();
             this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.nuUrunAdetBilgisi)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nuUrunKoliFiyati)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nuYeniUrunTaneFiyat)).BeginInit();
@@ -472,11 +473,21 @@
             this.label10.TabIndex = 32;
             this.label10.Text = "Adet Sayisi";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(633, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // YeniUrunDetayForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1286, 512);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label15);
             this.Controls.Add(this.pnUrunBilgileri);
             this.Controls.Add(this.txtBarkodNo);
@@ -533,5 +544,6 @@
         private System.Windows.Forms.NumericUpDown nuUrunKarOranı;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.NumericUpDown nuAdet;
+        private System.Windows.Forms.Button button1;
     }
 }
